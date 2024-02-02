7e62490e0e3ed55a1f80dc12f58f271abdef61cd
lanceenr <lancemikaelo.enriquez@gmail.com>
2024-02-02 11:40:25 +0800

    changes

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,6 @@
                 min="1"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Weight"
-                required=""
               />
             </div>
             <div>
@@ -56,7 +55,6 @@
                 type="number"
                 min="1"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required=""
                 placeholder="Height"
               />
             </div>
@@ -89,6 +87,14 @@
               <p class="text-xl text-yellow-600">{{ weightStatus }}</p>
             </h1>
           </div>
+          <div v-if="articleLink" class="mt-4">
+            <button
+              :onclick="'window.location.href=`' + articleLink + '`'"
+              class="w-full text-white bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Read more...
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -96,6 +102,8 @@
 </template>
 
 <script>
+import Swal from "sweetalert2";
+
 export default {
   data() {
     return {
@@ -105,10 +113,31 @@ export default {
     };
   },
   methods: {
-    calculateBMI() {
-      this.bmi = (this.weight / (this.height / 100) ** 2).toFixed(2);
+    async calculateBMI() {
+      if (this.weight && this.height) {
+        // check if weight and height are not null
+        let bmi = this.weight / (this.height / 100) ** 2;
+        if (isFinite(bmi)) {
+          // check if bmi is a finite number
+          this.bmi = bmi.toFixed(2);
+        } else {
+          this.bmi = null;
+          await Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Invalid input. Please enter valid numbers for weight and height.",
+          });
+        }
+      } else {
+        await Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Weight and height cannot be null. Please enter valid numbers.",
+        });
+      }
     },
   },
+
   computed: {
     weightStatus() {
       if (this.bmi === null) {
@@ -123,6 +152,15 @@ export default {
         return "Obese";
       } else {
         return "";
+      }
+    },
+    articleLink() {
+      if (this.weightStatus === "Obese" || this.weightStatus === "Overweight") {
+        return "https://www.medicalnewstoday.com/articles/303409";
+      } else if (this.weightStatus === "Underweight") {
+        return "https://www.medicalnewstoday.com/articles/321982";
+      } else {
+        return null;
       }
     },
   },
